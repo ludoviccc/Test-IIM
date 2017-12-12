71b98ef21d8ea979f54975cdc613a77881f6cc2e
Ludo <ludovic.ardin@devinci.fr>
2017-12-12 17:07:06 +0100

    bob v2

---
+++ bob.js
@@ -0,0 +1,6 @@
+function toggle(elemID){
+    var elem=document.getElementById(elemID);	
+    if(elem.style.display==='block'){
+        elem.style.display='none';	
+    }else{elem.style.display='block';}
+}
